d40b80e6ba971996455c528e15e3b8d2e76b92b8
Forrest P <lawfpenn@outlook.com>
2018-08-24 14:28:01 -0500

    Added initial change, no service worker

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoicGVsdCIsImEiOiJjamtuMnd2eWEwaG9zM3FxcnFxZjEwbDJjIn0.J-zbGnhW98bq02_2vCNv9g',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
